5eee2355d36301aadd85a8c41fb678f95c1e33b2
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-03-09 15:31:49 +0530

    Added video and Image ✨

--- app/src/main/java/com/example/tsechacksapp/doctor/MainActivityDoctor.java
+++ app/src/main/java/com/example/tsechacksapp/doctor/MainActivityDoctor.java
@@ -1,7 +1,7 @@
 package com.example.tsechacksapp.doctor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.tsechacksapp.R;
 import android.os.Bundle;
 
 public class MainActivityDoctor extends AppCompatActivity {
