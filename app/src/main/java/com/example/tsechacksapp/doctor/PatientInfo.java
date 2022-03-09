2fdd902f2ba0631c8db4d6e9ddc32c3b55bd8b13
ypdalvi <ypdalvi@gmail.com>
2022-03-09 15:52:03 +0530

    added doctor screen

--- app/src/main/java/com/example/tsechacksapp/doctor/PatientInfo.java
+++ app/src/main/java/com/example/tsechacksapp/doctor/PatientInfo.java
@@ -3,16 +3,12 @@ package com.example.tsechacksapp.doctor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
-import com.example.tsechacksapp.R;
-
-public class MainActivityDoctor extends AppCompatActivity {
-
+public class PatientInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_doctor);
+        setContentView(R.layout.activity_patient_info);
     }
 }
