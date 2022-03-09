fcfb2d36fd755ecd53c1f31a20314704d40a7509
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-03-10 03:07:23 +0530

    Added medicines ✨

--- app/src/main/java/com/example/tsechacksapp/data/PatientDetail.kt
+++ app/src/main/java/com/example/tsechacksapp/data/PatientDetail.kt
@@ -4,3 +4,8 @@ data class PatientDetail(var desc: String?="",
                          var status : String?="false" )
 
 data class PatientDetailList(var Fields: MutableList<PatientDetail>? = mutableListOf())
+
+
+
+data class MedicineDetail(var name: String?="", var morning: Boolean? = false, var evening: Boolean? = false,
+        var night: Boolean?=false, var quantity: String?="0 pills")
